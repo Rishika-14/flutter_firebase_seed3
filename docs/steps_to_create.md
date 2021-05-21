200cb215ad6af45daab40639919f560c10565aaf
Amit Kumar <amitgarg.cse@gmail.com>
2021-05-21 10:58:00 +0530

    Add android app for your firebase project

--- docs/steps_to_create.md
+++ docs/steps_to_create.md
@@ -1,13 +1,16 @@
 # Steps to create
 
-* Create flutter project from IDE
+* **Create flutter project from IDE**
     * run the code and test if it is working.
-* set up a github project and checkin the first code.
-* Create a firebase project.
-* Install dependencies.
+* **set up a github project and checkin the first code**.
+* **Create a firebase project**
+* **Install dependencies**
   * flutter pub add firebase_core 
   * flutter pub add firebase_auth 
   * flutter pub add google_sign_in 
   * flutter pub add flutter_bloc 
   * flutter pub add equatable 
   * flutter pub add cloud_firestore
+* **Add android app for your firebase project**
+  * ./gradlew signingReport
+  * bump the minSdkVersion to 21.
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp firebaseApp = await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -11,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -32,6 +26,13 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Hello world'),
+        ),
+        body: Center(
+          child: Text('Hello'),
+        )
+    );;
   }
 }
