b857714dd57312c0439f9f10988c4bbc1ca1c29c
Amit Kumar <amitgarg.cse@gmail.com>
2021-06-02 13:24:19 +0530

    Monthly Diet model

---
+++ lib/features/monthly_diet/model/monthly_diet.model.dart
@@ -0,0 +1,8 @@
+// import 'package:flutter_firebase_seed3/features/common_models/activity_model.dart';
+//
+// class MonthlyDietModel extends ActivityModel{
+//   //try to contain your changes in this folder.
+//
+//   String timePeriod;
+//   String markdownText;
+// }
