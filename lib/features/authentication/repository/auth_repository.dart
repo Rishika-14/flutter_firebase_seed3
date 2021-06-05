be78f9ba9888a81bae7ef46a5c468a2cbed22ea7
Amit Kumar <amitgarg.cse@gmail.com>
2021-06-05 12:59:24 +0530

    Removed Error reporting for auth repository, it should be added to auth_cubit

--- lib/features/authentication/repository/auth_repository.dart
+++ lib/features/authentication/repository/auth_repository.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart' as auth;
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -16,44 +15,27 @@ class AuthRepository extends BaseAuthRepository {
 
   @override
   Future<auth.User?> googleSignIn() async {
-    try {
-      final GoogleSignInAccount? googleSignInAccount =
-          await _googleSignIn.signIn();
+    final GoogleSignInAccount? googleSignInAccount =
+    await _googleSignIn.signIn();
 
-      if (googleSignInAccount == null) {
-        return null;
-      }
-
-      // Obtain the auth details from the request
-      final GoogleSignInAuthentication googleAuth =
-          await googleSignInAccount.authentication;
-
-      // Create a new credential
-      final credential = auth.GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      auth.UserCredential userCredential =
-          await _firebaseAuth.signInWithCredential(credential);
-
-      return userCredential.user;
-    } on auth.FirebaseAuthException catch (err) {
-      throw 'firebase-auth-exception';
-    } on PlatformException catch (err) {
-      print('Platform exception error '
-          '----------------------------------------------------->');
-      print('err.code ------>');
-      print(err.code);
-      print('err.message ------>');
-      print(err.message);
-      print('err.details ------>');
-      print(err.details);
-      print('err.stacktrace ------>');
-      print(err.stacktrace);
-      //TODO: change to Error Object
-      throw 'platform-exception';
+    if (googleSignInAccount == null) {
+      return null;
     }
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+    await googleSignInAccount.authentication;
+
+    // Create a new credential
+    final credential = auth.GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    auth.UserCredential userCredential =
+    await _firebaseAuth.signInWithCredential(credential);
+
+    return userCredential.user;
   }
 
   @override
